007f5e0116a8d3c67313326274f361668076c840
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-05 21:40:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,8 +13,12 @@ def link
   puts warning
 end 
 
-def any_phrase (phrase)
-  phrase = "Do A Barrel Roll!"
-  puts phrase
-end 
+# def any_phrase (phrase)
+#   phrase = "Do A Barrel Roll!"
+#   puts phrase
+# end 
 
+$phrase = "Do A Barrel Roll!"
+def any_phrase (phrase)
+  puts phrase
+end
